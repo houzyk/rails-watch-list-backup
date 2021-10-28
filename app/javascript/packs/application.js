b09993c795e52156c29daf1360a59b5cea857517
houzairkoussa <houzairmk@icloud.com>
2021-10-28 22:33:18 +0400

    added aos but does not function

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,13 +5,15 @@
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
+import AOS from 'aos';
 import "channels"
 import 'bootstrap';
 import { initSelect2 } from '../components/init_select2';
 import { loadDynamicBannerText } from '../components/banner';
-document.addEventListener("turbolinks:load", function() {
+document.addEventListener("turbolinks:load", () => {
   initSelect2();
   loadDynamicBannerText();
+  AOS.init();
 });
 Rails.start()
 Turbolinks.start()
