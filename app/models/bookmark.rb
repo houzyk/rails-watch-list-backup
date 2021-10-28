0427d549c3c636f0474fae29c653ba0cbd43504e
houzairkoussa <houzairmk@icloud.com>
2021-10-28 12:43:23 +0400

    removed 2nd bug from validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates :comment, length: { minimum: 6 }
-  validates :movie, uniqueness: { scope: %i[list movie] }
-  validates :list, uniqueness: { scope: %i[movie list] }
+  validates :movie, uniqueness: { scope: %i[list] }
+  validates :list, uniqueness: { scope: %i[movie] }
 end
